f5382699cbd0e1261bb218975982daef4217918d
Chris <chrishaining@yahoo.co.uk>
2020-02-25 15:41:05 +0000

    gets rid of multiple choice quiz, and has a simpler structure for the user

--- app/routes.py
+++ app/routes.py
@@ -4,30 +4,38 @@ from flask import render_template, redirect, request
 import random
 from sqlalchemy import update
 
-@app.route('/')
-def index():
+@app.route('/manage')
+def manage():
     topics = Topic.query.all()
-    return render_template('index.html', title='Home', topics=topics)
+    return render_template('manage.html', title='Manage', topics=topics)
 
-@app.route('/', methods=['POST'])
+@app.route('/manage', methods=['POST'])
 def create_topic():
     question = request.form['question']
     answer = request.form['answer']
     newTopic = Topic(question=question, answer=answer)
     db.session.add(newTopic)
     db.session.commit()
-    return redirect('/')
+    return redirect('/manage')
 
 # update a topic. This works. 
-@app.route('/<int:topic_id>/update', methods=['POST'])
-def update_topic(topic_id):
+@app.route('/manage/<int:topic_id>/update_answer', methods=['POST'])
+def update_answer(topic_id):
     newanswer = request.form.get("newanswer")
     topic_id = request.form.get("topic_id")
     topic = Topic.query.filter_by(id=topic_id).first()
     topic.answer = newanswer
     db.session.commit()
-    return redirect('/')
+    return redirect('/manage')
 
+@app.route('/manage/<int:topic_id>/update_question', methods=['POST'])
+def update_topic(topic_id):
+    newquestion = request.form.get("newquestion")
+    topic_id = request.form.get("topic_id")
+    topic = Topic.query.filter_by(id=topic_id).first()
+    topic.question = newquestion
+    db.session.commit()
+    return redirect('/manage')
 
 @app.route('/quiz')
 def show_quiz():
@@ -53,33 +61,33 @@ def answer_question(topic_id):
     return redirect('/quiz')
 
 # create random quizzes. It might help to have a TopicsBank class (i.e. all the questions) that has a random_quiz method. It would mean I could do less work in the routes. However, I'm not sure about the implications so I will stick with creating random quizzes through the routes. This method calculates the score (it might be better to put the score somewhere else).
-@app.route('/random_quiz')
-def show_random_quiz():
-    multis = MultipleChoiceTopic.query.all()
-    length = int(len(multis)/2)
-    quiz = random.sample(multis, length)
-    score=0
-    for topic in quiz:
-        if topic.outcome == True:
-            score +=1
-    return render_template('random_quiz.html', quiz=quiz, multis=multis, score=score)
+# @app.route('/random_quiz')
+# def show_random_quiz():
+#     multis = MultipleChoiceTopic.query.all()
+#     length = int(len(multis)/2)
+#     quiz = random.sample(multis, length)
+#     score=0
+#     for topic in quiz:
+#         if topic.outcome == True:
+#             score +=1
+#     return render_template('random_quiz.html', quiz=quiz, multis=multis, score=score)
 
 # @app.route('/random_quiz')
 # def score():
 #     score = 0
 #     return render_template('random_quiz.html', score=score)
 
-@app.route('/random_quiz', methods=['POST'])
-def create_multi_choice_topic():
-    question = request.form['question']
-    option_a = request.form['option_a']
-    option_b = request.form['option_b']
-    option_c = request.form['option_c']
-    answer = request.form['answer']
-    newMultiChoiceTopic = MultipleChoiceTopic(question=question, option_a=option_a, option_b=option_b, option_c=option_c, answer=answer)
-    db.session.add(newMultiChoiceTopic)
-    db.session.commit()
-    return redirect('/random_quiz')
+# @app.route('/random_quiz', methods=['POST'])
+# def create_multi_choice_topic():
+#     question = request.form['question']
+#     option_a = request.form['option_a']
+#     option_b = request.form['option_b']
+#     option_c = request.form['option_c']
+#     answer = request.form['answer']
+#     newMultiChoiceTopic = MultipleChoiceTopic(question=question, option_a=option_a, option_b=option_b, option_c=option_c, answer=answer)
+#     db.session.add(newMultiChoiceTopic)
+#     db.session.commit()
+#     return redirect('/random_quiz')
 
 
 # @app.route('/random_quiz/<int:multiple_choice_topic_id>', methods=['GET', 'POST'])
@@ -103,26 +111,26 @@ def create_multi_choice_topic():
     # db.session.commit()
     # return redirect('/random_quiz')
 
-@app.route('/random_quiz/<int:multiple_choice_topic_id>', methods=['GET', 'POST'])
-def submit_answer(multiple_choice_topic_id):
-    multiple_choice_topic = MultipleChoiceTopic.query.get(multiple_choice_topic_id)
-    multiple_choice_topic.user_answer = request.form['user_answer']
-    db.session.commit()
-    return redirect('/random_quiz')
+# @app.route('/random_quiz/<int:multiple_choice_topic_id>', methods=['GET', 'POST'])
+# def submit_answer(multiple_choice_topic_id):
+#     multiple_choice_topic = MultipleChoiceTopic.query.get(multiple_choice_topic_id)
+#     multiple_choice_topic.user_answer = request.form['user_answer']
+#     db.session.commit()
+#     return redirect('/random_quiz')
 
 # answer a multiple_choice_topic. 
-@app.route('/random_quiz/<int:multiple_choice_topic_id>/answer', methods=['POST'])
-def answer_multi_choice_question(multiple_choice_topic_id):
-    new_user_answer = request.form.get("new_user_answer")
-    multiple_choice_topic_id = request.form.get("multiple_choice_topic_id")
-    multiple_choice_topic = MultipleChoiceTopic.query.filter_by(id=multiple_choice_topic_id).first()
-    multiple_choice_topic.answer = new_user_answer
-    db.session.commit()
-    return redirect('/random_quiz')
+# @app.route('/random_quiz/<int:multiple_choice_topic_id>/answer', methods=['POST'])
+# def answer_multi_choice_question(multiple_choice_topic_id):
+#     new_user_answer = request.form.get("new_user_answer")
+#     multiple_choice_topic_id = request.form.get("multiple_choice_topic_id")
+#     multiple_choice_topic = MultipleChoiceTopic.query.filter_by(id=multiple_choice_topic_id).first()
+#     multiple_choice_topic.answer = new_user_answer
+#     db.session.commit()
+#     return redirect('/random_quiz')
 
 # flipcard fun
-@app.route('/flipper')
+@app.route('/home')
 def flipper():
     topics = Topic.query.all()
     card = random.choice(topics)
-    return render_template('flipcard.html', title='Flipper', topics=topics, card=card)
+    return render_template('home.html', title='Flipper', topics=topics, card=card)
